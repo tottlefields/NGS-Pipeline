fc7d002ecd7f7b802d68eae50f53cd9b3ddb1bef
eschofield <eschofield@gendev01>
2016-09-27 16:42:16 +0100

    split fastq2bam back into sep files as the bwa step requires multi-threading (4 cores) but the rest of the steps don't.

---
+++ shell/step2-sam2bam.sh
@@ -0,0 +1,39 @@
+#!/bin/bash -l
+
+#$ -b y
+#$ -cwd
+#$ -o logs/sam2bam.$TASK_ID.out
+#$ -e logs/sam2bam.$TASK_ID.err
+
+module load apps/picard
+module load apps/samtools
+module add apps/picard
+module add apps/samtools
+
+SAMPLE=$1
+ID=$(printf "%03d\n" $SGE_TASK_ID)
+
+
+#Check to see that bam file genereated in step 1 in >50MB
+bam_size=$(wc -c <" ${SAMPLE}_${ID}.bam")
+if [ $bam_size -le 50000000 ]
+then
+	rm - rf ${SAMPLE}_${ID}.sam
+fi
+
+
+samtools view -b -S -o ${RESULTS}/${SAMPLE}/${SAMPLE}_${ID}.bam ${RESULTS}/${SAMPLE}/${SAMPLE}_${ID}.sam
+picard AddOrReplaceReadGroups I=${SAMPLE}_${ID}.bam O=RG_${SAMPLE}_${ID}.bam RGID=${SAMPLE} RGLB=canfam3 RGPL='ILLUMINA' RGPU=${SAMPLE} RGSM=${SAMPLE} 
+picard ValidateSamFile I=RG_${SAMPLE}_${ID}.bam O=VAL_${SAMPLE}_${ID}.out MODE=SUMMARY
+
+
+bam_size=$(wc -c < ${RESULTS}/${SAMPLE}/${SAMPLE}_${ID}.bam)
+if [ $bam_size -ge 50000000 ]
+then
+	rm - rf ${RESULTS}/${SAMPLE}/${SAMPLE}_${ID}.sam
+fi
+
+
+# samtools view -b -S -o BC_29772_aligned_003.bam BC_29772_aligned_003.sam
+# picard AddOrReplaceReadGroups I=BC_29772_aligned_003.bam O=RG_BC_29772_003.bam RGID=BC_29772 RGLB=canfam3 RGPL='ILLUMINA' RGPU=BC_29772 RGSM=BC_29772 
+# picard ValidateSamFiles I=RG_BC_29772_003.bam O=VAL_BC_29772_003.out MODE=SUMMARY
