860e8c80295132aee661dee5c6df3a714d7a3ccc
Ellen Schofield <ellen.schofield@aht.org.uk>
2016-09-28 14:09:53 +0100

    correction on script name

--- shell/ngs-pipeline.sh
+++ shell/ngs-pipeline.sh
@@ -17,7 +17,7 @@ fi
 
 ## Convert chunked FASTQ files to aligned SAM files...
 qsub -V -t 1-3 -N ${SAMPLE}.fastq2bam ${PIPELINE_HOME}/step1-fastq2sam.sh ${SAMPLE}
-qsub -V -t 1-3 -hold_jid ${SAMPLE}.fastq2bam -N ${SAMPLE}.bamEdit ${PIPELINE_HOME}/step2-bamEdit.sh ${SAMPLE}
+qsub -V -t 1-3 -hold_jid ${SAMPLE}.fastq2bam -N ${SAMPLE}.sam2bam ${PIPELINE_HOME}/step2-sam2bam.sh ${SAMPLE}
 
 ## Convert chunked FASTQ files to aligned BAM files (via SAM)...
 #qsub -V -t 1-3 -N ${SAMPLE}.fastq2bam ${PIPELINE_HOME}/step1-fastq2bam.sh ${SAMPLE}
