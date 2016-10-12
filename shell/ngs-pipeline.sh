58fd830397ac195dd4b1ca9dce8d0fec7afaafa5
eschofield <eschofield@CPM207.aht.org.uk>
2016-10-12 09:10:06 +0100

    added FQ_COUNT variable

--- shell/ngs-pipeline.sh
+++ shell/ngs-pipeline.sh
@@ -6,6 +6,7 @@ export UPLOADS=$HOME/uploads
 export PIPELINE_HOME=$HOME/git/NGS-Pipeline/shell
 export ENSEMBL=86
 export PICARD_JAVA_OPTS=' -Xmx4g -Djava.io.tmpdir=${HOME}/javatmpdir'
+export SAM_COUNT=3
 SAMPLE=$1
 
 echo Starting NGS Pipeline for $SAMPLE...
@@ -18,8 +19,8 @@ then
 fi
 
 ## Convert chunked FASTQ files to aligned SAM files...
-qsub -V -t 1-3 -N ${SAMPLE}.fastq2sam ${PIPELINE_HOME}/step1-fastq2sam.sh ${SAMPLE}
-qsub -V -t 1-3 -hold_jid ${SAMPLE}.fastq2sam -N ${SAMPLE}.sam2bam ${PIPELINE_HOME}/step2-sam2bam.sh ${SAMPLE}
+qsub -V -t 1-${SAM_COUNT} -N ${SAMPLE}.fastq2sam ${PIPELINE_HOME}/step1-fastq2sam.sh ${SAMPLE}
+qsub -V -t 1-${SAM_COUNT} -hold_jid ${SAMPLE}.fastq2sam -N ${SAMPLE}.sam2bam ${PIPELINE_HOME}/step2-sam2bam.sh ${SAMPLE}
 
 ## Convert chunked FASTQ files to aligned BAM files (via SAM)...
 #qsub -V -t 1-3 -N ${SAMPLE}.fastq2bam ${PIPELINE_HOME}/step1-fastq2bam.sh ${SAMPLE}
