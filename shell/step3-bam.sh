b7bcf6f75ccad28c2d0254e165ae1088070b6787
eschofield <eschofield@CPM207.aht.org.uk>
2016-10-13 11:50:59 +0100

    changed size of files for deletion from 2GB to 200MB

--- shell/step3-bam.sh
+++ shell/step3-bam.sh
@@ -16,9 +16,9 @@ SAMPLE=$1
 echo "Merging BAM files"
 samtools merge ${SAMPLE}.raw.bam RG_${SAMPLE}_*.bam
 
-### if size of the merged bam file >= 2GB then delete corresponding bam files
+### if size of the merged bam file >= 200MB then delete corresponding bam files
 bam_size1=$(wc -c < ${RESULTS}/${SAMPLE}/${SAMPLE}.raw.bam)
-if [ $bam_size1 -ge 2000000000 ]; then
+if [ $bam_size1 -ge 200000000 ]; then
 	echo "Deleting ${RESULTS}/${SAMPLE}/RG_${SAMPLE}_*.bam"
 	rm - rf ${RESULTS}/${SAMPLE}/RG_${SAMPLE}_*.bam
 fi
@@ -26,7 +26,7 @@ fi
 
 picard SortSam I=${SAMPLE}.raw.bam O=${SAMPLE}.sorted.bam SORT_ORDER=coordinate
 
-### if size of the merged bam file >= 2GB then delete corresponding bam files
+### if size of the sorted bam file >= 10% of raw bam then delete file
 bam_size2=$(wc -c < ${RESULTS}/${SAMPLE}/${SAMPLE}.sorted.bam)
 if [ $bam_size2 -ge ${bam_size1}/10 ]; then
 	echo "Deleting ${RESULTS}/${SAMPLE}/${SAMPLE}.raw.bam"
@@ -36,7 +36,7 @@ fi
 
 picard MarkDuplicates I=${SAMPLE}.sorted.bam O=${SAMPLE}.bam VALIDATION_STRINGENCY=SILENT CREATE_INDEX=true M=MarkDuplicates_metrics.out
 
-### if size of the sorted bam file >= 2GB then delete corresponding bam files
+### if size of the bam file >= 10% of sorted bam then delete file
 bam_size3=$(wc -c < ${RESULTS}/${SAMPLE}/${SAMPLE}.bam)
 if [ $bam_size3 -ge ${bam_size2}/10 ]; then
 	echo "Deleting ${RESULTS}/${SAMPLE}/${SAMPLE}.sorted.bam"
