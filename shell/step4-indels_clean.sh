bd6ee20e324648177ca63fd56b60ba8ffca5d3f6
eschofield <eschofield@CPM207.aht.org.uk>
2016-11-28 09:42:29 +0000

    adding step4 to the pipeline

---
+++ shell/step4-indels_clean.sh
@@ -0,0 +1,46 @@
+#!/bin/bash -l
+
+#$ -b y
+#$ -cwd
+#$ -l h_vmem=6G
+#$ -pe smp 6
+#$ -o logs/step4.out
+#$ -e logs/step4.err
+#$ -m ae
+
+if [ $START_STEP -gt 4  ]; then
+	exit
+fi
+
+module load apps/picard
+
+SAMPLE=$1
+source ${RESULTS}/${SAMPLE}.cfg
+
+##RealignerTargetCreator
+gatk -T RealignerTargetCreator -nt 6 -R ${CF3}/canfam3.fasta -I ${SAMPLE}.bam -o ${SAMPLE}.bam.intervals -mismatch 0.0 -S LENIENT --filter_mismatching_base_and_quals
+
+##IndelRealigner
+gatk -T IndelRealigner -R ${CF3}/canfam3.fasta -I ${SAMPLE}.bam -targetIntervals ${SAMPLE}.bam.intervals -o ${SAMPLE}.clean.bam -S LENIENT --filter_mismatching_base_and_quals
+
+### if size of the clean bam file >= 10% of bam then delete file
+bam_size=$(wc -c < ${RESULTS}/${SAMPLE}/${SAMPLE}.bam)
+bam_size1=$(wc -c < ${RESULTS}/${SAMPLE}/${SAMPLE}.clean.bam)
+if [ $bam_size1 -ge $(( ${bam_size}/10 )) ]; then
+	echo "Deleting ${RESULTS}/${SAMPLE}/${SAMPLE}.bam"
+	rm -rf ${RESULTS}/${SAMPLE}/${SAMPLE}.bam
+fi
+
+
+##BaseRecalibrator
+gatk -T BaseRecalibrator -nct 6 -R ${CF3}/canfam3.fasta -i ${SAMPLE}.clean.bam -knownSites ${CF3}/canfam3_all_snps.vcf -o ${SAMPLE}.recal.grp -S LENIENT
+
+##PrintReads
+gatk -T PrintReads -nct 6 -R ${CF3}/canfam3.fasta -I ${SAMPLE}.clean.bam -BQSR ${SAMPLE}.recal.grp -o ${SAMPLE}.final.bam -S LENIENT
+
+### if size of the final bam file >= 10% of clean bam then delete file
+bam_size2=$(wc -c < ${RESULTS}/${SAMPLE}/${SAMPLE}.final.bam)
+if [ $bam_size1 -ge $(( ${bam_size1}/10 )) ]; then
+	echo "Deleting ${RESULTS}/${SAMPLE}/${SAMPLE}.clean.bam"
+	rm -rf ${RESULTS}/${SAMPLE}/${SAMPLE}.clean.bam
+fi
