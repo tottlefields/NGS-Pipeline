ddff27bf62841b2b33017a6cb1f82f20c75d3740
Ellen Schofield <ellen.schofield@aht.org.uk>
2016-12-15 10:15:13 +0000

    corrected line endings

--- shell/split_fastq.sh
+++ shell/split_fastq.sh
@@ -7,8 +7,12 @@ cd ${FASTQ}
 mkdir ${SAMPLE}
 
 for f in `ls *${SAMPLE}*fastq*`; do 
-	echo $f; 
+	echo $f;
+	FQ_FILE=`cut -f 1 -d '.' $f`
+	echo zcat $f | split -l 4000000 --additional-suffix=".fastq" --numeric-suffixes=1 --suffix-length=3 --filter='gzip > $FILE.gz' - ${FASTQ}/${SAMPLE}/${FQ_FILE}_
 done
 
+echo ${SAMPLE} fastq files have been aplit into a new directory - ${FASTQ}/${SAMPLE};
+echo;
 
 #zcat ../BC_29772_R1.fastq.gz | split -l 4000000 --additional-suffix=".fastq" --numeric-suffixes=1 --suffix-length=3 --filter='gzip > $FILE.gz' - BC_29772_R1_
